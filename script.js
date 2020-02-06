2e7dd1c33c7dcdf48111cd0448ace66a001b2d30
zhabkOo <freem.ua@gmail.com>
2020-02-07 01:24:43 +0200

    some code

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-alert("Hello world!");
+let userSymbol = prompt("Type random symbol:");
+// console.log(userSymbol, userSymbol.length);
+
+if (userSymbol != null) {
+  switch (userSymbol == "" || userSymbol.length > 1) {
+    case true:
+      userSymbol = " ";
+      console.log("nothing or > 1. userSymbol:", userSymbol, typeof userSymbol);
+      break;
+    case false:
+      userSymbol = userSymbol.toLowerCase();
+      console.log(
+        "if !nothing or <= 1. userSymbol:",
+        userSymbol,
+        typeof userSymbol
+      );
+      break;
+    default:
+      alert("aaa");
+  }
+} else {
+  alert("Ohrana otmena");
+}
+
+switch (userSymbol) {
+  case "a":
+  case "e":
+  case "o":
+    userSymbol = userSymbol.toUpperCase();
+    console.log(userSymbol);
+    break;
+  case "x":
+  case "y":
+    alert(userSymbol + userSymbol);
+    break;
+}

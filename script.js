fea541a56e9199544a6ca972cbda36a813d824af
zhabkOo <freem.ua@gmail.com>
2020-02-07 02:35:05 +0200

    some f*cking code and boolean whitespace problem

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let userSymbol = prompt("Type random symbol:");
 // console.log(userSymbol, userSymbol.length);
 
 if (userSymbol != null) {
-  switch (userSymbol == "" || userSymbol.length > 1) {
+  switch (userSymbol === "" || userSymbol.length > 1) {
+    // case " ":
+    //     alert(!!userSymbol);
+    //     break;
     case true:
       userSymbol = " ";
       console.log("nothing or > 1. userSymbol:", userSymbol, typeof userSymbol);
@@ -16,7 +19,8 @@ if (userSymbol != null) {
       );
       break;
     default:
-      alert("aaa");
+      alert("Something went wrong, pleasy try again");
+      break;
   }
 } else {
   alert("Ohrana otmena");
@@ -32,5 +36,33 @@ switch (userSymbol) {
   case "x":
   case "y":
     alert(userSymbol + userSymbol);
+    break;
+  case "s":
+  case "d":
+  case "q":
+  case "m":
+    var randomNumAmountTrunc = Math.trunc(Math.random() + Math.random());
+    console.log(
+      "Округленная сумма двух случайных чисел randomNumAmountTrunc = ",
+      randomNumAmountTrunc
+    );
+    switch (randomNumAmountTrunc != 0) {
+      case true:
+        console.log(
+          "Переменная уже создана со значением, удовлетворяющим условиям: randomNumAmountTrunc = ",
+          randomNumAmountTrunc
+        );
+        break;
+      default:
+        randomNumAmountTrunc = 1;
+        console.log(
+          "Сгенерирован 0, значит записываем дефолтный 1: randomNumAmountTrunc =",
+          randomNumAmountTrunc
+        );
+        break;
+    }
+    break;
+  default:
+    alert(Math.max(Math.random(), Math.random(), Math.random()));
     break;
 }
